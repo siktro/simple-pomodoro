eecf238f6745dc2d9f64050e8db42b5c613e7733
siktro <siktro@gmail.com>
2021-02-26 21:15:07 +0300

    Some changes to fix layout

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// one config to rule them all
 const config = {
   imgPath: './assets/digits.png',
   unitWidth: 150,
   unitHeight: 190,
+  animationTime: 0.5,
   // allow time overflow? 1:30 == 90
   // allow auto increment higher 
   // time format hh:mm:ss
   // hide hours if not clock?
   // hide leading zeros
 }
+
+const filterByUnitType = filter => elem => elem.dataset.unitType === filter
 
 function loadImage( url ) {
   return new Promise((resolve, reject) => {
@@ -26,7 +30,7 @@ function calcUnitPivots( width, height ) {
   return [ wp, hp ]
 }
 
-function generateCssAnimations ( heightPivots ) {
+function generateCssAnimations( heightPivots ) {
   const result = []
 
   // "head to tail" shift
@@ -39,13 +43,13 @@ function generateCssAnimations ( heightPivots ) {
   // generate css animations with different steps
   // each animation will start one step "later" than previous
   let prev = heightPivots
-  const fraction = Math.floor(100 / heightPivots.length) // each step amount
+  const fraction = Math.ceil(100 / heightPivots.length) // each step amount
   for (let i = 0; i < heightPivots.length; ++i) {
 
     const domStr = `
       @keyframes#set-cycle-${i} {
         ${prev.reduce((acc, el, idx) => {
-          acc += `${(idx+1) * fraction}% { background-position-y: ${el}px; }`
+          acc += `${Math.min((idx+1) * fraction, 100)}% { background-position-y: ${el}px; }`
           return acc
         }, '')} 
       }
@@ -64,8 +68,26 @@ function createEmptyStyleSheet() {
   return css.sheet
 }
 
-function attachAnimation( sheet, rules ) {
+function attachAnimation( elementsArr, rulesAmount ) {
+  const postConfig = `${config.animationTime}s infinite steps(1, jump-start)`
 
+  const generateAnimationName = x => `set-cycle-${x % rulesAmount}`
+  const generateConfigStr = x => `${generateAnimationName(x)} ${postConfig}`
+
+  // attach animation to digit symbols
+  elementsArr
+    .filter(filterByUnitType('digit'))
+    .forEach((elem, i) => elem.style.animation = generateConfigStr(i))
+  
+  elementsArr
+    .filter(filterByUnitType('divider'))
+    .forEach((elem, i) => elem.style.animation = generateConfigStr(i))
+}
+
+function fixNonDigitSymbols( elementsArr, widthPivots ) {
+  elementsArr
+    .filter(filterByUnitType('divider'))
+    .forEach(elem => elem.style.backgroundPositionX = `-${widthPivots[10]}px`)
 }
 
 function displayClock( groups, widthPivots ) {
@@ -85,7 +107,22 @@ function displayClock( groups, widthPivots ) {
       ones.style.backgroundPositionX = `-${widthPivots[+part[1]]}px`
     }
   )
+}
 
+function attachListeners() {
+  const root = document.getElementById('root')
+  const controls = document.getElementById('controls')
+
+  root.addEventListener('mouseenter', () => {
+    // if (!document.hasFocus()) return
+    console.log('hello')
+    // controls.style.display = 'block'
+  })
+
+  root.addEventListener('mouseleave', () => {
+    console.log('bye')
+    // controls.style.display = ''
+  })
 }
 
 async function initApplication() {
@@ -103,24 +140,17 @@ async function initApplication() {
   cssAnimations.forEach(rule => styleSheet.insertRule(rule))
 
   const unitElements = Array.from(document.querySelectorAll('[data-unit-type]'))
-  // attaching style animations
-  unitElements
-    .forEach((elem, i) => elem.style.animation = `set-cycle-${i % heightPivots.length} 1s infinite steps(1, jump-start)`)
-
-  // fixnig non-digit symbols X position
-  unitElements
-    .filter(elem => elem.dataset.unitType === 'divider')
-    .forEach(elem => elem.style.backgroundPositionX = `-${widthPivots[10]}px`)
+  attachAnimation( unitElements, heightPivots.length )
+  fixNonDigitSymbols( unitElements, widthPivots )
 
   const groups = ['hours', 'minutes', 'seconds'].reduce((arr, id) => {
     const group = document.getElementById(id)
     arr.push([group.firstElementChild, group.lastElementChild])
     return arr
   }, [])
-
-  setInterval(() => {
-    displayClock( groups, widthPivots )
-  }, 1000)
 }
 
-initApplication()
+// initApplication()
+//   .then(value => {
+//     attachListeners()
+//   })
